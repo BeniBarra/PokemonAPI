4865ba13231c727da0d01fc1272442cbd77bda33
Benjamin Ibarra <Loco.queens@gmail.com>
2022-10-28 08:22:53 -0400

    refined pokemon constructor and optimized cache useage for newly created response data

--- queryHandler/pokemonConstructor.js
+++ queryHandler/pokemonConstructor.js
@@ -7,59 +7,69 @@ let cache = require('./cache')
 let key = 'pokemonObj';
 
 class Pokemon {
-    constructor(name, id, moves, sprites, stats, types){
+    constructor(name, id, moves, sprites, description, stats, types){
         this.name = name;
         this.id = id;
         this.moves = moves;
         this.sprites = sprites;
+        this.description = description;
         this.stats = stats;
         this.types = types;
     }
 }
 
 let pokemonConstructor = async (req, res) => {
-    let pokemonFromCache = cache['pokemon151'];
+    if(!cache[key]){
+        console.log('Constructor Cache Miss');
 
-    let newPokemonArr = [] 
+        let pokemonFromCache = cache['pokemon151'];
 
-    let counter = 0
-
-    pokemonFromCache.forEach( async (data) => {
-
-        let queryData = await axios.get(data.url);
-
-        let pokemonData = queryData.data;
-
-        //console.log(pokemonData);
-
-        let pokemonName = pokemonData.name;
-
-        let pokemonId = pokemonData.id;
-
-        let pokemonMoves = pokemonData.moves.map((moves) => moves.move.name);
-
-        let pokemonSprites = pokemonData.sprites.other['official-artwork'].front_default;
-
-        let pokemonStats = pokemonData.stats.map((stats) => ({name: stats.stat.name, baseStat: stats.base_stat}))
-
-        let pokemonTypes = pokemonData.types.map((types) => ({name: types.type.name}))
-
-        const newPokemon = new Pokemon(pokemonName, pokemonId, pokemonMoves, pokemonSprites, pokemonStats, pokemonTypes);
+        let newPokemonArr = [];
     
-        newPokemonArr.push(newPokemon);
+        let counter = 0;
+    
+        pokemonFromCache.forEach( async (data) => {
+            
+            let queryData = await axios.get(data.url);
 
-        counter++;
+            let pokemonData = queryData.data;
 
-        if(counter == pokemonFromCache.length) {
-            newPokemonArr.sort((a,b) => (a.id > b.id) ? 1 : -1)
-            cache[key] = newPokemonArr;
-        }
-    });
+            let descriptionQueryData = await axios.get(queryData.data.species.url);
+    
+            let descriptionData = descriptionQueryData.data;
+    
+            //console.log(pokemonData);
+    
+            let pokemonName = pokemonData.name;
+    
+            let pokemonId = pokemonData.id;
+    
+            let pokemonMoves = pokemonData.moves.map((moves) => moves.move.name);
+    
+            let pokemonSprites = pokemonData.sprites.other['official-artwork'].front_default;
 
-        console.log(cache[key]);
-    // cache[key] = newPokemonArr;
-    // console.log(cache[key]);
-    res.send(cache[key]);
+            let pokemonDescription = descriptionData.flavor_text_entries[0].flavor_text.replace(/[\r\n\f]/gm, ' ');
+    
+            let pokemonStats = pokemonData.stats.map((stats) => ({name: stats.stat.name, baseStat: stats.base_stat}));
+    
+            let pokemonTypes = pokemonData.types.map((types) => ({name: types.type.name}));
+    
+            const newPokemon = new Pokemon(pokemonName, pokemonId, pokemonMoves, pokemonSprites, pokemonDescription, pokemonStats, pokemonTypes);
+        
+            newPokemonArr.push(newPokemon);
+            counter++;
+    
+            if(counter == pokemonFromCache.length) {
+                newPokemonArr.sort((a,b) => (a.id > b.id) ? 1 : -1);
+                cache[key] = newPokemonArr;
+                console.log(cache[key]);
+                res.send(cache[key]);
+            }
+        });
+    } else {
+        console.log('Constructor Cache Hit');
+        res.send(cache[key]);
+    }
 }
 
 
